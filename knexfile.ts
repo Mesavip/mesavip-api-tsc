5c8e5f7aeeedf797fe1b13393a5654df88992530
Daniel Marques <daniel.brz2009@gmail.com>
2021-05-21 16:18:47 -0300

    deploy stuff

--- knexfile.ts
+++ knexfile.ts
@@ -5,31 +5,71 @@ import { resolve } from 'path';
 // pg.defaults.ssl = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    ssl: { rejectUnauthorized: false },
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      extension: 'ts',
+      tableName: 'knexmigrations',
+      directory: resolve(
+        __dirname,
+        'src',
+        'shared',
+        'infra',
+        'knex',
+        'migrations'
+      ),
+    },
+    seeds: {
+      directory: resolve(__dirname, 'src', 'shared', 'infra', 'knex', 'seeds'),
+    },
   },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    extension: 'ts',
-    tableName: 'knexmigrations',
-    directory: resolve(
-      __dirname,
-      'src',
-      'shared',
-      'infra',
-      'knex',
-      'migrations'
-    ),
-  },
-  seeds: {
-    directory: resolve(__dirname, 'src', 'shared', 'infra', 'knex', 'seeds'),
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      extension: 'js',
+      tableName: 'knexmigrations',
+      directory: resolve(
+        __dirname,
+        'dist',
+        'src',
+        'shared',
+        'infra',
+        'knex',
+        'migrations'
+      ),
+    },
+    seeds: {
+      directory: resolve(
+        __dirname,
+        'dist',
+        'src',
+        'shared',
+        'infra',
+        'knex',
+        'seeds'
+      ),
+    },
   },
 };
