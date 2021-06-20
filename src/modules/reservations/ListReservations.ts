055d7cdb50ac04212bc90e3392b22200984ff1c1
Daniel Marques <daniel.brz2009@gmail.com>
2021-06-20 14:39:10 -0300

    Listing now past and future reservations

--- src/modules/reservations/ListReservations.ts
+++ src/modules/reservations/ListReservations.ts
@@ -5,8 +5,23 @@ import query from '../../shared/infra/knex/knex';
 class ListReservations {
   async execute(request: Request, response: Response): Promise<Response> {
     const { id: client_id } = request.user;
+    const date = new Date();
+    const today = date.toISOString();
 
-    const reservations = await query
+    const pastReservations = await query
+      .select([
+        'r.reservation_id as id',
+        'u.name as restaurant',
+        query.raw(`to_char(r.date, 'Mon dd, yyyy') as date`),
+        'h.hour',
+      ])
+      .from({ r: 'reservations' })
+      .innerJoin({ h: 'hours' }, 'r.hour_id', 'h.hour_id')
+      .innerJoin({ u: 'users' }, 'u.user_id', 'h.restaurant_id')
+      .where({ 'r.client_id': client_id, 'r.canceled': null })
+      .andWhere('r.date', '<', today);
+
+    const followingReservations = await query
       .select([
         'r.reservation_id as id',
         'u.name as restaurant',
@@ -20,13 +35,16 @@ class ListReservations {
       .innerJoin({ h: 'hours' }, 'r.hour_id', 'h.hour_id')
       .innerJoin({ u: 'users' }, 'u.user_id', 'h.restaurant_id')
       .innerJoin({ a: 'addresses' }, 'a.user_id', 'h.restaurant_id')
-      .where({ 'r.client_id': client_id, 'r.canceled': null });
+      .where({ 'r.client_id': client_id, 'r.canceled': null })
+      .andWhere('r.date', '>', today);
 
-    if (!reservations.length) {
-      return response.status(400).json({ error: 'No reservations were found' });
+    if (!pastReservations && !followingReservations.length) {
+      return response.status(403).json({ error: 'No reservations were found' });
     }
 
-    return response.status(201).json(reservations);
+    return response
+      .status(200)
+      .json({ pastReservations, followingReservations });
   }
 }
 export { ListReservations };
