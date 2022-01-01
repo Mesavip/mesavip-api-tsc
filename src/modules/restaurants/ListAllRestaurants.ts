bee31079def8501961f15d860a5aea9dadf454e6
Daniel Marques <daniel.brz2009@gmail.com>
2022-01-01 12:03:25 -0300

    all

--- src/modules/restaurants/ListAllRestaurants.ts
+++ src/modules/restaurants/ListAllRestaurants.ts
@@ -4,6 +4,9 @@ import query from '../../shared/infra/knex/knex';
 
 class ListAllRestaurants {
   async execute(request: Request, response: Response): Promise<Response> {
+    const { name } = request.params;
+    const restaurant_name = name ? name : '';
+
     const restaurants = await query
       .select([
         'r.id',
@@ -17,8 +20,11 @@ class ListAllRestaurants {
       .innerJoin({ c: 'culinaries' }, 'c.id', 'r.culinary_id')
       .innerJoin({ a: 'addresses' }, 'a.restaurant_id', 'r.id')
       .innerJoin({ f: 'files' }, 'r.id', 'f.restaurant_id')
-      .innerJoin({ rat: 'ratings' }, 'rat.restaurant_id', 'r.id')
+      .leftJoin({ rat: 'ratings' }, 'rat.restaurant_id', 'r.id')
       .where({ 'f.type': 'list' })
+      //It's done like this, because of the issue below
+      //https://github.com/knex/knex/issues/1207#issuecomment-185079698
+      .whereRaw(`upper(r.name) like upper('%'||?||'%')`, [restaurant_name])
       .groupBy(['r.id', 'f.id', 'c.id', 'a.id']);
 
     if (!restaurants.length) {
--- src/shared/infra/http/routes/restaurants.routes.ts
+++ src/shared/infra/http/routes/restaurants.routes.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import { ListAllRestaurants } from '../../../../modules/restaurants/ListAllRestaurants';
 import { ListAvailableHours } from '../../../../modules/restaurants/ListAvailableHours';
 import { ListOneRestaurant } from '../../../../modules/restaurants/ListOneRestaurant';
-// import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const restaurantsRoutes = Router();
 
@@ -11,8 +10,8 @@ const listOneRestaurant = new ListOneRestaurant();
 const listAllRestaurants = new ListAllRestaurants();
 const listAvailableHours = new ListAvailableHours();
 
-restaurantsRoutes.get('/', listAllRestaurants.execute);
-restaurantsRoutes.get('/:restaurant_id', listOneRestaurant.execute);
+restaurantsRoutes.get('/:name?', listAllRestaurants.execute);
+restaurantsRoutes.get('/list-by-id/:restaurant_id', listOneRestaurant.execute);
 
 restaurantsRoutes.get(
   '/available-hours/:restaurant_id/:date',
