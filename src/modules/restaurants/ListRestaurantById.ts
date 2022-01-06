16f69b5445e0548209361ebe484a4e2ee34672c1
Daniel Marques <daniel.brz2009@gmail.com>
2022-01-06 01:04:51 -0300

    all

--- src/modules/restaurants/ListRestaurantById.ts
+++ src/modules/restaurants/ListRestaurantById.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 import query from '../../shared/infra/knex/knex';
 
-class ListOneRestaurant {
+class ListRestaurantById {
   async execute(request: Request, response: Response): Promise<Response> {
     const { restaurant_id } = request.params;
 
@@ -12,24 +12,33 @@ class ListOneRestaurant {
         'r.about',
         'r.phone',
         'r.site',
-        'c.name as culinary',
-        'a.cep',
-        'a.logradouro',
-        'a.numero',
-        'a.complemento',
-        query.raw(`concat_ws(' - ', a.bairro, a.cidade, a.estado) as address`),
+        'r.culinary',
+        query.raw(
+          `json_build_object(
+            'bairro',a.bairro,
+            'cidade', a.cidade,
+            'estado', a.estado,
+            'cep', a.cep,
+            'logradouro', a.logradouro,
+            'numero', a.numero,
+            'complemento', a.complemento
+            )as address`
+        ),
         query.raw('cast(avg(rat.rating) as decimal(10,1)) AS avg_rating'),
         query.raw(
-          `concat_ws(' - ', to_char(r.opening_hour, 'HH24:MI'), to_char(r.closing_hour, 'HH24:MI')) as operation_hours`
+          `concat_ws(
+          ' - ',
+          to_char(r.opening_hour, 'HH24:MI'),
+          to_char(r.closing_hour, 'HH24:MI')
+          ) as operation_hours`
         ),
       ])
       .count('rat.rating as total_ratings')
       .from({ r: 'restaurants' })
-      .innerJoin({ c: 'culinaries' }, 'c.id', 'r.culinary_id')
       .innerJoin({ a: 'addresses' }, 'a.restaurant_id', 'r.id')
       .innerJoin({ rat: 'ratings' }, 'rat.restaurant_id', 'r.id')
       .where({ 'r.id': restaurant_id })
-      .groupBy(['r.id', 'c.id', 'a.id'])
+      .groupBy(['r.id', 'a.id'])
       .first();
 
     if (!restaurant) {
@@ -39,4 +48,4 @@ class ListOneRestaurant {
     return response.status(200).json(restaurant);
   }
 }
-export { ListOneRestaurant };
+export { ListRestaurantById };
--- src/shared/infra/http/routes/restaurants.routes.ts
+++ src/shared/infra/http/routes/restaurants.routes.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 
 import { ListAllRestaurants } from '../../../../modules/restaurants/ListAllRestaurants';
 import { ListAvailableHours } from '../../../../modules/restaurants/ListAvailableHours';
-import { ListOneRestaurant } from '../../../../modules/restaurants/ListOneRestaurant';
+import { ListRestaurantById } from '../../../../modules/restaurants/ListRestaurantById';
 
 const restaurantsRoutes = Router();
 
-const listOneRestaurant = new ListOneRestaurant();
+const listRestaurantById = new ListRestaurantById();
 const listAllRestaurants = new ListAllRestaurants();
 const listAvailableHours = new ListAvailableHours();
 
 restaurantsRoutes.get('/:name?', listAllRestaurants.execute);
-restaurantsRoutes.get('/list-by-id/:restaurant_id', listOneRestaurant.execute);
+restaurantsRoutes.get('/list-by-id/:restaurant_id', listRestaurantById.execute);
 
 restaurantsRoutes.get(
   '/available-hours/:restaurant_id/:date',
