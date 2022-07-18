6a8fb731ecc4de2b0789a330200065a46f4d98e3
Daniel Marques <daniel.brz2009@gmail.com>
2022-07-18 01:53:46 -0300

    return total cuisines on the lsit cuisines query

--- src/modules/restaurants/ListCuisines.ts
+++ src/modules/restaurants/ListCuisines.ts
@@ -3,9 +3,21 @@ import query from '../../shared/infra/knex/knex';
 
 export class ListCuisines {
   async execute(request: Request, response: Response): Promise<Response> {
-    const cuisines = await query('restaurants')
-      .distinct('culinary as name')
-      .limit(6);
+    const cuisines = await query
+      .raw(
+        `
+        SELECT DISTINCT
+        culinary AS name,
+        (
+          SELECT COUNT(r2.id)
+          FROM restaurants r2
+          WHERE r2.culinary = r.culinary
+        ) AS total
+        FROM restaurants r
+        GROUP BY id
+      `
+      )
+      .then((res) => res.rows);
 
     if (!cuisines.length) {
       return response.status(404).json({ error: 'Cuisines not found' });
